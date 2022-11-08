3c1212f026b0e47cb737722c02ff7654ebb98094
Christopher Baum <christopherabaum@gmail.com>
2022-11-08 16:36:41 -0600

    Change 3 strings and add console.log commands

--- variables.js
+++ variables.js
@@ -9,3 +9,18 @@ var hsGrad = 2013
 var socks = true
 var shoes = false
 var feetCold = false
+
+mood = "Ready for a nap"
+waterBottle = "Half empty"
+weather = "Beautiful sunny day :)"
+
+
+console.log(mood)
+
+console.log(waterBottle)
+
+console.log(feetCold)
+
+console.log(luckyNum)
+
+console.log(feetCold)
