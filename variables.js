dcf08e2381768b01ada7f3c672884a851fb52606
Christopher Baum <christopherabaum@gmail.com>
2022-11-08 16:49:43 -0600

    Add semicolons for proper conventions

--- variables.js
+++ variables.js
@@ -1,26 +1,26 @@
-var mood = "I'm hungry"
-var waterBottle = "Half full"
-var weather = "Cloudy and rainy :("
+var mood = "I'm hungry";
+var waterBottle = "Half full";
+var weather = "Cloudy and rainy :(";
 
-var age = 27
-var luckyNum = 13
-var hsGrad = 2013
+var age = 27;
+var luckyNum = 13;
+var hsGrad = 2013;
 
-var socks = true
-var shoes = false
-var feetCold = false
+var socks = true;
+var shoes = false;
+var feetCold = false;
 
-mood = "Ready for a nap"
-waterBottle = "Half empty"
-weather = "Beautiful sunny day :)"
+mood = "Ready for a nap";
+waterBottle = "Half empty";
+weather = "Beautiful sunny day :)";
 
 
-console.log(mood)
+console.log(mood);
 
-console.log(waterBottle)
+console.log(waterBottle);
 
-console.log(weather)
+console.log(weather);
 
-console.log(luckyNum)
+console.log(luckyNum);
 
-console.log(feetCold)
+console.log(feetCold);
